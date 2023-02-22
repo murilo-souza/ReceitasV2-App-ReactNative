0d923aedaebe3bbf19d18509d5e4a4cd68539889
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-02-22 15:28:08 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native'
+import { Landing } from './src/screens/Landing'
+
+export default function App() {
+  return (
+    <View>
+      <Landing />
+    </View>
+  )
+}
---
+++ src/screens/Landing/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export function Landing(){
+  return (
+    <Container>
+      <Text>Landing</Text>
+    </Container>
+  )
+}
