3341610e1168fa9a679cbece95873ae5d21793fb
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-03-01 21:51:05 -0300

    base config to validade inputs in new recipe screen

--- src/screens/NewRecipe/index.tsx
+++ src/screens/NewRecipe/index.tsx
@@ -6,8 +6,27 @@ import { InputLarge } from '../../components/InputLarge'
 import { Button } from '../../components/Button'
 import { TypeButton } from '../../components/TypeButton'
 import { Cookie, CookingPot } from 'phosphor-react-native'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const schema = yup.object().shape({
+  title: yup.string().required('Título é obrigatório'),
+  description: yup.string().required('Descrição é obrigatória'),
+  ingredients: yup.string().required('Ingredientes é obrigatória'),
+  prepare: yup.string().required('Preparação é obrigatório'),
+})
 
 export function NewRecipe() {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
   return (
     <Container>
       <Header title="Criar receita" />
