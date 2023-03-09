5c9c09ebfbb5a0b4f3ae7a1637f138bcf29c0cca
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-03-09 16:02:45 -0300

    create a context provider

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,6 +1,19 @@
-import { createContext } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 interface AuthContextData {
   name: string
 }
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
 const AuthContext = createContext({} as AuthContextData)
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [name, setName] = useState('')
+
+  return (
+    <AuthContext.Provider value={{ name }}>{children}</AuthContext.Provider>
+  )
+}
