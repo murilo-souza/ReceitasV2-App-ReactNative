111dcddb038ac4baae7572a4e4471eb5d9c76c91
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-03-12 19:58:27 -0300

    trying to get google sigin work

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -5,6 +5,8 @@ import { Envelope } from 'phosphor-react-native'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { RootParamListPublic } from '../../routes/public.routes'
+import { useEffect } from 'react'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 type Props = StackNavigationProp<
   RootParamListPublic,
@@ -16,6 +18,19 @@ export function Landing() {
 
   function handleSignInWithEmailAndPassword() {
     navigation.navigate('signInWithEmailAndPassword')
+  }
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '985080108657-4g73um7h4l49kkbj9t3euj3kun98mbdi.apps.googleusercontent.com',
+    })
+  }, [])
+
+  async function handleSigninWithGoogle() {
+    await GoogleSignin.hasPlayServices()
+    const userInfo = await GoogleSignin.signIn()
+    console.log(userInfo)
   }
 
   return (
@@ -31,7 +46,11 @@ export function Landing() {
         perca
       </Description>
       <AccessText>Acesse agora</AccessText>
-      <SignInButton icon={GoogleSVG} title="Entrar com Google" />
+      <SignInButton
+        icon={GoogleSVG}
+        title="Entrar com Google"
+        onPress={handleSigninWithGoogle}
+      />
       <SignInButton
         icon={Envelope}
         title="Entrar com Email e senha"
